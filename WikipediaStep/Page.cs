d15f6a07ea2e2ebee22ad185704f21df4fcc08cd
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-13 17:33:16 +0100

    method GetResponse

--- WikipediaStep/Page.cs
+++ WikipediaStep/Page.cs
@@ -7,4 +7,21 @@ public class Page
 
     // Constructors
     public string url => $"https://en.wikipedia.org/wiki/{title}";
+    public List<string> urlList = new List<string>();
+    public HttpResponseMessage response;
+    
+    // Methods
+    static public HttpResponseMessage GetResponse(string url)
+    {
+        HttpClient client = new HttpClient(); client.DefaultRequestHeaders.Add("User-Agent", "WikiPath/1.0 (Educational Project)");
+        HttpResponseMessage response = new HttpResponseMessage();
+        
+        Thread process = new Thread(() =>
+        {
+            response = client.GetAsync(url).Result;
+        });
+        process.Start();
+        process.Join();
+        return response;
+    }
 }
