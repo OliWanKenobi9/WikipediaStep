f5088ef3744dd916a15c2feb8b8204eb1bb7d2e2
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-13 17:24:29 +0100

    input

--- WikipediaStep/Program.cs
+++ WikipediaStep/Program.cs
@@ -2,18 +2,27 @@
 
 internal class Program
 {
-    void Input()
+    static void Input(out Page origin, out Page destination)
     {
+        origin = new Page();
+        destination = new Page();
         switch (EnvironmetProperties.Debug)
         {
             case true:
+                origin.title = "Lando_Norris";
+                destination.title = "McLaren";
                 break;
             case false:
+                Console.Write("Origin: ");
+                origin.title = Console.ReadLine();
+                
+                Console.Write("Destination: ");
+                destination.title = Console.ReadLine();
                 break;
         }
     }
     static void Main(string[] args)
     {
-        
+        Input(out Page origin, out Page destination);
     }
 }
