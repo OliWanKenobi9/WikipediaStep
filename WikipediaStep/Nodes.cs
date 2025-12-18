8f1bda143afbd8f7ce14e5bf26412afe0e10d307
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-18 12:11:12 +0100

    List instead of Array

--- WikipediaStep/Nodes.cs
+++ WikipediaStep/Nodes.cs
@@ -3,12 +3,13 @@ namespace WikipediaStep;
 public class Nodes
 {
     public Page origin { get; set; }
-    public Page[] node => new Page[origin.urlList.Count];
+    public List<Page> node;
 
     public void Worker(Page origin)
     {
         for (int i = 0; i < origin.urlList.Count; i++)
         {
+            node.Add(new Page());
             node[i].url = origin.urlList[i];
             node[i].response = node[i].GetResponse(node[i].url);
         }
