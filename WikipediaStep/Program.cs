b0710a8615db46966b67e99e1f2dbb45f513a0cb
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-19 08:59:46 +0100

    fixes

--- WikipediaStep/Program.cs
+++ WikipediaStep/Program.cs
@@ -10,7 +10,7 @@ internal class Program
         {
             case true:
                 origin.url = "https://en.wikipedia.org/wiki/Theodosius_III";
-                destination.url = "https://en.wikipedia.org/wiki/Sebastos\n";
+                destination.url = "https://en.wikipedia.org/wiki/Sebastos";
                 break;
             case false:
                 Console.Write("Origin: ");
@@ -29,37 +29,44 @@ internal class Program
         //          i = 0
 
         bool found = false;
+        int j = 0;
         while (found == false)
         {
             for (int i = 0; i < nodes.node.Count; i++)
             {
                 if (nodes.node[i].urlList.Contains(destination))
                 {
+                    Console.WriteLine($"Done searching node {i}: {nodes.node[i].url}");
                     Console.WriteLine($"Destination found in node {i}: {nodes.node[i].url}");
                     Environment.Exit(0);
                 }
 
                 Console.WriteLine($"Done searching node {i}: {nodes.node[i].url}");
             }
-            Console.WriteLine("Done searching nodes");
+            Console.WriteLine($"Done searching nodes {j}");
+            j++;
         }
     }
     
     static void Main(string[] args)
     {
+        /*
+         * TODO: Fix    ORIGIN-> NODE ->DESTINATION path to
+         *              ORIGIN-> NODE * x -> DESTINATION
+         */
         Input(out Page origin, out Page destination);
-        Nodes testnodes = new Nodes();
+        Nodes nodes = new Nodes();
         // Declaration
         
         origin.response = origin.GetResponse(origin.url);
         origin.urlList = origin.ExtractURL();
-
+        
         destination.response = destination.GetResponse(destination.url);
         destination.urlList = destination.ExtractURL();
         
-        Thread process = new Thread(() => NodeSearch(destination.url, testnodes));
+        Thread process = new Thread(() => NodeSearch(destination.url, nodes));
         process.Start();
         
-        testnodes.Worker(origin);
+        nodes.Worker(origin);
     }
 }
